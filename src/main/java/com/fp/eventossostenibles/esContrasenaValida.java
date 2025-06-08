3bf9f7d5608c08bdf473fea653ecbc91fc9501a6
jmorque <jmorque1111@g.educaand.es>
2025-06-08 18:44:41 +0200

    Delete src directory

--- src/main/java/com/fp/eventossostenibles/esContrasenaValida.java
+++
@@ -1,7 +0,0 @@
-package org.iesvdmProyectoMain;
-
-public class esContrasenaValida {
-	 public static boolean esContrasenaValida(String contrasena) {
-	        return contrasena != null && contrasena.length() >= 8;
-	    }
-}
